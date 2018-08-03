f0936b9868bd5fb97e4433c3af617f07e166afc9
brightscannon <brightscannon@gmail.com>
2018-08-03 14:45:44 +0900

    Add project : recommend system - article analysis tid

---
+++ choocheunsystem/article/article.py
@@ -0,0 +1,36 @@
+from flask import *
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+app.config.update(
+    TEMPLATES_AUTO_RELOAD = True, # 이것은 템플릿이 수정되면 자동으로 리로드함..
+)
+
+models = {}
+
+def init():
+    with open("./models/classification.pkl", "rb") as f:
+        models["classification"] = pickle.load(f)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route("/predic")
+def predic():
+    result = {}
+    result["category"] = ["정치","경제","사회","생활/문화","세계","IT/과학"]
+
+    model = models["classification"]
+
+    sentence = request.values.get("sentence")
+
+    
+
+
+    return jsonify(result)
+
+
+init()
+app.run()
